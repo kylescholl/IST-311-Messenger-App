e2e43c16d1ddbf72a171401c3707bee566e679f1
kylescholl <kyle.b.scholl@gmail.com>
2020-04-20 20:09:12 -0400

    Big boi commit 2

---
+++ src/messenger/ForgotPasswordSceneController.java
@@ -0,0 +1,29 @@
+package messenger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Justin
+ */
+public class ForgotPasswordSceneController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
