83c9e1ed7aeb9e13a5146f90a82b4f13b1abe019
kylescholl <kyle.b.scholl@gmail.com>
2020-04-29 15:50:33 -0400

    Commit

--- src/messenger/Main.java
+++ src/messenger/Main.java
@@ -7,12 +7,9 @@ package messenger;
  */
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 
