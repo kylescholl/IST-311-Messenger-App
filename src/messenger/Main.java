a1e533f6f93233c3cc80bb46fb4608b1b8197378
kylescholl <kyle.b.scholl@gmail.com>
2020-04-29 17:32:44 -0400

    Commit

--- src/messenger/Main.java
+++ src/messenger/Main.java
@@ -1,10 +1,5 @@
 package messenger;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -14,7 +9,6 @@ import javafx.stage.Stage;
 
 
 /**
- *
  * @author sscho
  */
 public class Main extends Application {
