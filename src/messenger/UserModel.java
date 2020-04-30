60bf459454419131ce1a07233f04497b246375f4
kylescholl <kyle.b.scholl@gmail.com>
2020-04-30 13:29:41 -0400

    Commit

---
+++ src/messenger/UserModel.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package messenger;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "SampleModel")
+@NamedQueries({
+    @NamedQuery(name = "SampleModel.findAll", query = "SELECT s FROM SampleModel s"),
+    @NamedQuery(name = "SampleModel.findById", query = "SELECT s FROM SampleModel s WHERE s.id = :id"),
+    @NamedQuery(name = "SampleModel.findByValue", query = "SELECT s FROM SampleModel s WHERE s.value = :value")})
+public class UserModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Long id;
+    @Column(name = "Value")
+    private String value;
+
+    public SampleModel() {
+    }
+
+    public SampleModel(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof SampleModel)) {
+            return false;
+        }
+        SampleModel other = (SampleModel) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ist311project.SampleModel[ id=" + id + " ]";
+    }
+    
+}
