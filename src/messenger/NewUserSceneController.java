70a405c3eeb0bf1f6b01fcad75092bb3b575cac0
Justin Kelly <justink124@outlook.com>
2020-04-30 14:05:34 -0400

    updated new user confirmation

--- src/messenger/NewUserSceneController.java
+++ src/messenger/NewUserSceneController.java
@@ -7,7 +7,12 @@ package messenger;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 
 /**
  * FXML Controller class
@@ -16,6 +21,11 @@ import javafx.fxml.Initializable;
  */
 public class NewUserSceneController implements Initializable {
 
+    @FXML
+    private Button continueButton;
+    @FXML
+    private Hyperlink signInLink;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +33,24 @@ public class NewUserSceneController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void continueButtonPress(ActionEvent event) {
+        try {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information Dialog");
+            alert.setHeaderText("Welcome!");
+            alert.setContentText("New Account Created");
+            alert.showAndWait();
+
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
+    }
+
+    @FXML
+    private void signInAction(ActionEvent event) {
+        // go to login scene
+    }
     
 }
