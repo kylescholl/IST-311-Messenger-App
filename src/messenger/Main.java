17c3d53d9489a58cd13234ba12e04e5242643a95
kylescholl <kyle.b.scholl@gmail.com>
2020-05-01 15:56:18 -0400

    wip

--- src/messenger/Main.java
+++ src/messenger/Main.java
@@ -17,7 +17,6 @@ public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) throws Exception {
-        
         window = primaryStage;
         
         // loading from FXML
