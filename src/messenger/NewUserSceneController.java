6fcb0d1f8ef6ca212e0390b50f634f969cd63ef7
kylescholl <kyle.b.scholl@gmail.com>
2020-05-01 00:15:50 -0400

    NewUserSceneController.java: - Comments and planning
    NewUserScene.fxml:
    - UI and code connection corrections

--- src/messenger/NewUserSceneController.java
+++ src/messenger/NewUserSceneController.java
@@ -20,62 +20,87 @@ import javafx.stage.Stage;
 
 /**
  * NewUserSceneController Class
+ *
  * @author sscho
  */
 public class NewUserSceneController implements Initializable {
-
+    
     @FXML
     private Button continueButton;
-
+    
     @FXML
     private PasswordField passwordField;
-
+    
     @FXML
     private PasswordField confirmPasswordField;
-
+    
     @FXML
     private TextField emailField;
-
+    
     @FXML
-    private Hyperlink signInLink;
-
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-
+    private Hyperlink loginSegueButton;
+    
     @FXML
     private void continueButtonPress(ActionEvent event) {
+        /**
+         * TODO:
+         * Verify proper data entry --> email must be in correct format
+         * Verify email doesn't already exist in system
+         * 
+         * Send new user's data to DB
+         * Notify user of success (done)
+         * Close or transition back to login scene
+         *  --> if unable to find a solution, just have them close it (see below)
+         * 
+         * PROBLEM: Old login scene will not have new user's information unless queried again
+         * POSSIBLE SOLUTION: Close old window when transitioning to new scene
+         */
+        
+        
+        // This will only happen when a new user is successfully registered //
         try {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Information Dialog");
             alert.setHeaderText("Welcome!");
             alert.setContentText("New Account Created");
             alert.showAndWait();
-
         } catch (Exception ex) {
             System.err.println(ex);
         }
     }
-
+    
+    /**
+     * Return to LoginScene
+     */
     @FXML
-    private void signInAction(ActionEvent event) {
+    private void loginSegue(ActionEvent event) {
+        /**
+         * Would simply just closing the window work?
+         * Problems:
+         *  Old scene may not have most current data from database
+         *  Only works when returning to previous scene, wouldn't work when going to a new scene
+         */
         FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginScene.fxml"));
-            Parent secondRoot = null;
+        Parent secondRoot = null;
         try {
             secondRoot = loader.load();
         } catch (IOException ex) {
             Logger.getLogger(NewUserSceneController.class.getName()).log(Level.SEVERE, null, ex);
         }
-            
-            // Show Second FXML in new a window            
-            Stage stage = new Stage();
-            stage.setScene(new Scene(secondRoot));
-            stage.setTitle("Login Window");
-            stage.show();
-            // loading from FXML
+        
+        // Show Second FXML in new a window            
+        Stage stage = new Stage();
+        stage.setScene(new Scene(secondRoot));
+        stage.setTitle("Login Window");
+        stage.show();
+        // loading from FXML
+    }
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
     }
 }
